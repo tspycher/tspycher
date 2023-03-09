33075ae531d7b51605bec93d447573346f17bdaf
Thomas Spycher <me@tspycher.com>
2023-03-09 15:15:46 +0100

    BunPath fix
    updated bun_path

--- pcconfig.py
+++ pcconfig.py
@@ -5,5 +5,6 @@ import os
 config = pc.Config(
     app_name="tspycher",
     api_url=os.environ.get("API_URL", "http://127.0.0.1:8000"),
+    bun_path=os.environ.get("BUN_PATH", pc.constants.BUN_PATH), # on Docker Image '/root/.bun/bin/bun'
     db_url="sqlite:///pynecone.db",
 )
