00250e66c517187e80a025a32999bfee2aee83ec
Thomas Spycher <me@tspycher.com>
2023-03-22 11:31:40 +0100

    some corrections and CSS adjustments

--- tspycher/pages/__init__.py
+++ tspycher/pages/__init__.py
@@ -1,35 +1,32 @@
 import pynecone as pc
 
 
-
 class BasePage(object):
     def _header(self) -> pc.Component:
-        #return pc.container(
-            #pc.menu(
-            #    pc.menu_button(pc.icon(tag="hamburger")),
-            #    pc.menu_list(
-            #        pc.menu_item("Social Media", is_focusable=False, is_disabled=True),
-            #        pc.menu_item(pc.link("LinkedIn ", pc.icon(tag="external_link"), href="https://www.linkedin.com/in/tspycher84/", is_external=True)),
-            #        pc.menu_item(pc.link("Instagram ", pc.icon(tag="external_link"), href="https://www.instagram.com/tspycher/", is_external=True)),
-            #        pc.menu_divider(),
-            #        pc.menu_item("API's", is_focusable=False, is_disabled=True),
-            #        pc.menu_item(pc.link("LSZI API", href="http://localhost:8000/api/airfield/lszi", is_external=True)),
-            #    ),
-            #),
-            #pc.spacer(),
-            #,
-            #width="100%",
-            #height="10vh",
-            #padding="1.0em"
-        #)
+        # return pc.container(
+        # pc.menu(
+        #    pc.menu_button(pc.icon(tag="hamburger")),
+        #    pc.menu_list(
+        #        pc.menu_item("Social Media", is_focusable=False, is_disabled=True),
+        #        pc.menu_item(pc.link("LinkedIn ", pc.icon(tag="external_link"), href="https://www.linkedin.com/in/tspycher84/", is_external=True)),
+        #        pc.menu_item(pc.link("Instagram ", pc.icon(tag="external_link"), href="https://www.instagram.com/tspycher/", is_external=True)),
+        #        pc.menu_divider(),
+        #        pc.menu_item("API's", is_focusable=False, is_disabled=True),
+        #        pc.menu_item(pc.link("LSZI API", href="http://localhost:8000/api/airfield/lszi", is_external=True)),
+        #    ),
+        # ),
+        # pc.spacer(),
+        # ,
+        # width="100%",
+        # height="10vh",
+        # padding="1.0em"
+        # )
         return pc.hstack(
             pc.box(
-                #pc.text("LEFT"),
                 width="33%",
                 text_align="left",
             ),
             pc.box(
-                #pc.text("CENTER"),
                 width="33%",
                 text_align="center",
             ),
@@ -37,6 +34,7 @@ class BasePage(object):
                 pc.button(
                     pc.icon(tag="sun"),
                     on_click=pc.toggle_color_mode,
+                    name="themechange"
                 ),
                 width="33%",
                 text_align="right",
@@ -55,8 +53,9 @@ class BasePage(object):
             self._footer(),
             center_content=True,
             justifyContent="center",
-            maxWidth="auto",
+            maxWidth="100vw",
             height="100vh",
+            width="100vw",
             background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
         )
 
@@ -65,18 +64,52 @@ class BasePage(object):
 
     def _footer(self) -> pc.Component:
         return pc.vstack(
-            pc.center(
-            "Made by myself, build with ", pc.link(pc.button("pynecone"), href="https://pynecone.io", is_external=True),
-            " Framework, powered by ", pc.link(pc.button("FastAPI"), href="https://fastapi.tiangolo.com", is_external=True),
-            " and run with ", pc.link(pc.button("Google Cloud Run"), href="https://cloud.google.com/run", is_external=True),
-            #" and run with ", pc.link(pc.button("AWS ECS"), href="https://aws.amazon.com/ecs/", is_external=True),
-            " as a ", pc.link(pc.button("Docker"), href="https://www.docker.com", is_external=True), " image",
-            "... oh boy i love ", pc.link(pc.button("Python"), href="https://www.python.orgm", is_external=True),
+            pc.desktop_only(
+                pc.box(
+                    "This landing-page is so over-engineered but i had fun building it.",
+                    text_align="center",
+                    white_space="normal"
+                )
             ),
+            pc.desktop_only(
+                pc.box(
+                    "It's made by myself, build with ",
+                    pc.link(pc.button("pynecone"), href="https://pynecone.io", is_external=True, name="pynecone"),
+                    " Framework, powered by ",
+                    pc.link(pc.button("FastAPI"), href="https://fastapi.tiangolo.com", is_external=True,
+                            name="fastapi"),
+                    " and run with ",
+                    pc.link(pc.button("Google Cloud Run"), href="https://cloud.google.com/run", is_external=True,
+                            name="cloudrun"),
+                    # " and run with ", pc.link(pc.button("AWS ECS"), href="https://aws.amazon.com/ecs/", is_external=True, name="ecs"),
+                    " as a ",
+                    pc.link(pc.button("Docker"), href="https://www.docker.com", is_external=True, name="docker"),
+                    " image",
+                    "... oh boy i love ",
+                    pc.link(pc.button("Python"), href="https://www.python.org", is_external=True, name="python"),
+                    text_align="center",
+                    white_space="normal"
+                )
+            ),
+            pc.mobile_and_tablet(
+                pc.box(
+                    "This landing-page is so over-engineered with",
+                    pc.link(pc.button("Docker"), href="https://www.docker.com", is_external=True, name="docker"), ", ",
+                    pc.link(pc.button("Google Cloud Run"), href="https://cloud.google.com/run", is_external=True,
+                            name="cloudrun"), " and many more stuff. But i had fun building it.",
+                    text_align="center",
+                    white_space="normal"
+                )
+            ),
+
+            center_content=True,
             width="100%",
             bottom=0,
             padding="2.0em",
-            position="absolute"
+            position="absolute",
+            white_space="normal",
+            display="flex",
+            color="DimGrey"
         )
 
 
--- tspycher/tspycher.py
+++ tspycher/tspycher.py
@@ -9,8 +9,8 @@ page_landing = PageLandingpage()
 app = pc.App(
     state=State,
 )
-app.add_page(page_landing.body, route="/")
-app.add_page(page_home.body, route="/home/")
+app.add_page(page_landing.body, route="/", title="Tom Spycher", description="Welcome on my over-engineered web site", on_load=pc.toggle_color_mode)
+app.add_page(page_home.body, route="/home/", title="Tom Spycher", description="Welcome on my over-engineered web site", on_load=pc.toggle_color_mode)
 app.api.add_api_route("/", api_status)
 app.api.add_api_route("/airfield/{name}", api_airfield)
 app.compile()
