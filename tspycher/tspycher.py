63d0b7442811f4b831f0dda73d52405aef373ccd
Thomas Spycher <me@tspycher.com>
2023-10-04 11:47:41 +0200

    fixed an issue with TypeError: Cannot convert Var 'toggleColorMode' to bool

--- tspycher/tspycher.py
+++ tspycher/tspycher.py
@@ -10,8 +10,8 @@ page_landing = PageLandingpage()
 app = rx.App(
     state=State,
 )
-app.add_page(page_landing.body, route="/", title="Tom Spycher", description="Welcome on my over-engineered website", on_load=rx.toggle_color_mode)
-app.add_page(page_home.body, route="/home/", title="Tom Spycher", description="Welcome on my over-engineered website", on_load=rx.toggle_color_mode)
+app.add_page(page_landing.body, route="/", title="Tom Spycher", description="Welcome on my over-engineered website")#, on_load=rx.toggle_color_mode)
+app.add_page(page_home.body, route="/home/", title="Tom Spycher", description="Welcome on my over-engineered website")#, on_load=rx.toggle_color_mode)
 
 app.api.add_api_route("/", api_status)
 app.api.add_api_route("/teltonika", api_teltonika_gps, methods=["POST"])
