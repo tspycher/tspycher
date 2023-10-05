aec78986bd8768f8e211ee943a911f72ba3f66e6
Thomas Spycher <me@tspycher.com>
2023-10-05 08:17:04 +0200

    fixing the root_path

--- tspycher/main.py
+++ tspycher/main.py
@@ -1,20 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI(
     title="tspycher API",
     description="api's for my Life",
-    root_path="/api"
+    root_path="/api",
 )
+router = APIRouter(
+    prefix="/api",
+    tags=["Api"],
+    responses={404: {"description": "Not found"}}
+)
+
+
 
 #from tspycher.api import api_airfield, api_teltonika_gps, api_teltonika_latest
 from tspycher.api import api_airfield
 
 
-@app.get("/")
+@router.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 #app.add_api_route("/teltonika", api_teltonika_gps, methods=["POST"])
 #app.add_api_route("/teltonika/latest", api_teltonika_latest, methods=["GET"])
-app.add_api_route("/airfield/{name}", api_airfield)
+router.add_api_route("/airfield/{name}", api_airfield)
+
+app.include_router(router)
