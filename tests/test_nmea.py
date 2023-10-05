25636f289cab4be8c11b813968796901c734c847
Thomas Spycher <me@tspycher.com>
2023-10-05 09:02:29 +0200

    refactored the last missing api endpoint too

--- tests/test_nmea.py
+++ tests/test_nmea.py
@@ -1,11 +1,21 @@
 import os
-from . import client
+from fastapi.testclient import TestClient
+from tspycher.main import app
+
+client = TestClient(app)
 
 testpayload_file = os.path.join(os.path.dirname(__file__), "testpayload.txt")
 testpayload = open(testpayload_file, "rb").read()
 
 
-def test_tracker():
-    response = client.post("/teltonika", params={"imei": 123456789012345, "serial_num": 123456789012345}, data=testpayload)
+def test_tracker_latest():
+    response = client.get("/api/teltonika/latest")
+    data = response.json()
     assert response.status_code == 200
-    assert response.json() == {"Status": "Received"}
+    assert data['id']
+
+def test_tracker():
+    response = client.post("/api/teltonika/", params={"imei": 123456789012345, "serial_num": 123456789012345}, data=testpayload)
+    data = response.json()
+    assert response.status_code == 200
+    assert data.get("status") == "Received"
--- tspycher/api/teltonika.py
+++ tspycher/api/teltonika.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import Session
 from tspycher.libs.nmea import parse
 from tspycher.database import get_db
 from tspycher.api.models.track import TeltonikaTrack, TeltonikaTrackSchema
+import logging
+from pydantic import BaseModel
+
+
+logger = logging.getLogger("uvicorn.run")
 
 router = APIRouter(
     prefix="/teltonika",
@@ -10,15 +15,18 @@ router = APIRouter(
     responses={404: {"description": "Not found"}}
 )
 
+class NmeaResponseSchema(BaseModel):
+    status: str
+    points_added: int = 0
+
 @router.get("/latest", response_model=TeltonikaTrackSchema)
 async def api_teltonika_latest(request: Request, db: Session = Depends(get_db)):
     result = db.query(TeltonikaTrack).order_by(TeltonikaTrack.timestamp.desc()).limit(1).first()
     return result
 
-
-"""
-async def api_teltonika_gps(imei:int, serial_num:int, request: Request):
-    print(f"Received NMEA Data from IMEI: {imei} with Serial Number: {serial_num}")
+@router.post("/", response_model=NmeaResponseSchema)
+async def api_teltonika_gps(imei:int, serial_num:int, request: Request, db: Session = Depends(get_db)):
+    logger.info(f"Received NMEA Data from IMEI: {imei} with Serial Number: {serial_num}")
 
     payload = await request.body()
     nmea = parse(payload)
@@ -30,7 +38,7 @@ async def api_teltonika_gps(imei:int, serial_num:int, request: Request):
             if not data.did_geo_change(previous):
                 continue
         previous = data
-        print(f"Received NMEA Data: {data.datetime} {data.latitude_decimal_degrees} {data.latitude_direction} {data.longitude_decimal_degrees} {data.longitude_direction} {data.kmh} {data.track} {data.num_satellites} {data.altitude}")
+        logger.info(f"Received NMEA Data: {data.datetime} {data.latitude_decimal_degrees} {data.latitude_direction} {data.longitude_decimal_degrees} {data.longitude_direction} {data.kmh} {data.track} {data.num_satellites} {data.altitude}")
         teltonika_tracks.append(TeltonikaTrack(timestamp=data.datetime,
                        latitude_decimal_degrees=data.latitude_decimal_degrees, latitude_direction=data.latitude_direction,
                        longitude_decimal_degrees=data.longitude_decimal_degrees, longitude_direction=data.longitude_direction,
@@ -42,10 +50,7 @@ async def api_teltonika_gps(imei:int, serial_num:int, request: Request):
                        mobile_serial_num=serial_num))
 
     
-    with rx.session() as session:
-        session.bulk_save_objects(teltonika_tracks)
-        session.commit()
-    
+    db.add_all(teltonika_tracks)
+    db.commit()
 
-    return {"Status": "Received"}
-"""
+    return {"status": "Received", "points_added": len(teltonika_tracks)}
